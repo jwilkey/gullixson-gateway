05617e3a0d5fd3e14563533ee0ab4ca3897808c2
Justin Wilkey <jwilkey@Justins-MacBook-Pro.local>
2018-11-26 00:04:04 -0500

    Add appointment status

--- routes/users.js
+++ routes/users.js
@@ -9,15 +9,15 @@ router.get('/:id/appointments', (req, res) => {
 module.exports = router
 
 const appointments333 = {
-  pending: [{date: '2018-12-30', time: '17:33'}],
-  declined: [{date: '2018-10-30', time: '9:33'}],
+  pending: [{date: '2018-12-30', time: '17:33', status: 'pending'}],
+  declined: [{date: '2018-10-30', time: '9:33', status: 'declined'}],
   scheduled: [],
-  completed: [{date: '2018-11-12', time: '7:33'}, {date: '2018-11-17', time: '9:33'}]
+  completed: [{date: '2018-11-12', time: '7:33', status: 'completed'}, {date: '2018-11-17', time: '9:33', status: 'completed'}]
 }
 
 const appointments222 = {
   pending: [],
   declined: [],
-  scheduled: [{date: '2018-12-28', time: '11:33'}, {date: '2019-2-8', time: '10:43'}],
+  scheduled: [{date: '2018-12-28', time: '11:33', status: 'scheduled'}, {date: '2019-2-8', time: '10:43', status: 'scheduled'}],
   completed: []
 }
