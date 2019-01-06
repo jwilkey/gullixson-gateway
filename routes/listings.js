1ebfc6d222e197a23ec597570f2d5a45d20a5b2d
Justin Wilkey <jwilkey@Justins-MacBook-Pro.local>
2019-01-05 23:41:38 -0500

    /listings returns all images

--- routes/listings.js
+++ routes/listings.js
@@ -4,7 +4,15 @@ const request = require('request')
 
 router.get('/', (req, res) => {
   const listingsUrl = 'https://www.gullixson.com/ajax/request?class=property.api&api=/Properties/search'
-  const searchOptions = { 'filter': { 'where': { 'ListingType': ['House', 'Condo'], 'Status': { 'inq': ['Active', 'Contract'] } }, 'fields': ['_id', '_Class', 'MlsStatus', 'PropertyType', 'PropertySubType', 'UnparsedAddress', 'StreetNumberNumeric', 'StreetName', 'StreetSuffix', 'UnitNumber', 'City', 'CountyOrParish', 'StateOrProvince', 'ListPrice', 'ClosePrice', 'ListingId', 'ListingKey', 'BathroomsFull', 'BedroomsTotal'], 'limit': 20, 'skip': 0, 'include': { 'relation': 'Images', 'scope': { 'order': 'Order', 'fields': ['MediaURL'], 'limit': 1 } }, 'count': true } }
+  const searchOptions = {
+    'filter': {
+      'where': { 'ListingType': ['House', 'Condo'], 'Status': { 'inq': ['Active', 'Contract'] } },
+      'fields': ['_id', '_Class', 'MlsStatus', 'PropertyType', 'PropertySubType', 'UnparsedAddress', 'StreetNumberNumeric', 'StreetName', 'StreetSuffix', 'UnitNumber', 'City', 'CountyOrParish', 'StateOrProvince', 'ListPrice', 'ClosePrice', 'ListingId', 'ListingKey', 'BathroomsFull', 'BathroomsHalf', 'PublicRemarks', 'BedroomsTotal'],
+      'limit': 20,
+      'skip': 0,
+      'include': { 'relation': 'Images', 'scope': { 'order': 'Order', 'fields': ['MediaURL'] } },
+      'count': true }
+  }
 
   request({
     url: listingsUrl,
